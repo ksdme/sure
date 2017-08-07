6f4f2747d0723a028959d98c43e1d498f2cea1cc
Kilari Teja <kilariteja9@gmail.com>
2017-08-07 23:05:13 +0530

    Fixed exceptions, defaults to empty messages

--- sure/exceptions.py
+++ sure/exceptions.py
@@ -1,4 +1,10 @@
 """
-	@author ksdme
-	Exception Classes for Sure
+    @author ksdme
+    Exception Classes for Sure
 """
+
+class SureTypeError(Exception):
+    """ Sure TypeError """
+
+    def __init__(self, msg=""):
+        super(SureTypeError, self).__init__(msg)
