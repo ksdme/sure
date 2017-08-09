f41a9f61bb10941b6318070ed89b4cea026dffce
Kilari Teja <kilariteja9@gmail.com>
2017-08-09 22:28:48 +0530

    Updated Constants

--- sure/utilities.py
+++ sure/utilities.py
@@ -3,6 +3,15 @@
     Contains Utilities
 """
 
+class _DUMB(object):
+    """ simply used to build refs """
+
+    def __str__(self):
+        return self.msg
+
+    def __init__(self, msg):
+        self.msg = msg
+
 class Consts(object):
     """
         Constants,
@@ -10,11 +19,22 @@ class Consts(object):
         instead of value comparison
     """
 
-    # Fail Signal
-    Fail = ["Failed"]
+    # Used only in cases when a test fails
+    Fail = _DUMB("Failed")
+
+    # Used to represent bool vals
+    Faalse = _DUMB("False")
+    Truee = _DUMB("True")
+
+    @staticmethod
+    def bool(val):
+        if val is False:
+            return Consts.Faalse
+
+        return Consts.Truee
 
     # Pass Signal
-    Pass = ["Passed"]
+    Pass = _DUMB("Passed")
 
     # Undefined yet
-    Undefined = ["Undefined"]
+    Undefined = _DUMB("Undefined")
