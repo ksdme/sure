7cafc635d0375780b26aec083176500f6a3e23a3
Kilari Teja <kilariteja9@gmail.com>
2017-08-20 23:24:30 +0530

    Added nested test units

--- tests/units/builder.py
+++ tests/units/builder.py
@@ -3,14 +3,14 @@
     tests the sure.builder module
 """
 from unittest import TestCase
-from sure.builder import prop
 from sure.exceptions import SureTypeError
 from sure.types import integer, string, positive
+from sure.builder import prop, nested_prop, nested
 
 class TestBuilder(TestCase):
     """ tests builder """
 
-    def test_builder(self):
+    def test_flat_builder(self):
         """ builder class """
 
         class Sample(object):
@@ -29,3 +29,44 @@ class TestBuilder(TestCase):
 
         with self.assertRaises(SureTypeError):
             sample.serial = -15014
+
+    def test_nested_builder(self):
+        """ tests nested builder """
+
+        class Human(object):
+            family = nested_prop({
+                "father": nested({
+                    "name": string().length(5),
+                    "age": integer().positive()}),
+
+                "mother": nested({
+                    "name": string().length(5),
+                    "age": integer().positive(),
+                    "family": nested({
+                        "father": string()
+                    })})
+            })
+
+        mine = Human()
+
+        mine.family.father = {
+            "name": "Kilari",
+            "age": 25
+        }
+
+        mine.family.mother = {
+            "family": {
+                "father": "dad"
+            }
+        }
+
+        self.assertEqual(mine.family.father.age, 25)
+        self.assertEqual(mine.family.mother.family.father, "dad")
+
+        with self.assertRaises(SureTypeError):
+            mine.family.father.age = "20"
+
+        with self.assertRaises(SureTypeError):
+            mine.family.father = {
+                "age": "20"
+            }
