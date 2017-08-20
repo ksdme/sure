31cb2c424b965e8a7c6acb787ee6890fdfc1986a
Kilari Teja <kilariteja9@gmail.com>
2017-08-20 23:24:09 +0530

    Added nested_prop() and nested()

--- sure/builder.py
+++ sure/builder.py
@@ -6,7 +6,38 @@
 from time import time
 from random import randrange
 from sure.utilities import Consts, u_resolve_fail
-from sure.exceptions import SureTypeError, SureValueError
+from sure.exceptions import SureTypeError, TypeDefinitionError, SureValueError
+
+class StaticModel(object):
+    """
+        parent class for all static type
+        holders
+    """
+
+    def __init__(self, deep=None, **kargs):
+
+        # set unnested attributes
+        for name, val in kargs.iteritems():
+            setattr(self, name, val)
+
+        # set deep internal stuff
+        for name, val in deep:
+            name, context = name.strip().split("."), self
+            for elm in name[:-1]:
+                context = getattr(context, elm)
+
+            setattr(context, name[-1], val)
+
+def _gen_random_name():
+    """
+        generates random name
+    """
+
+    val_name = str(randrange(10000))
+    val_name += "{0:.20f}".format(time()).replace(".", "")
+    val_name = "_var" + val_name
+
+    return val_name
 
 def gen_prop_getter(val_name, setter, throws):
     """
@@ -22,7 +53,11 @@ def gen_prop_getter(val_name, setter, throws):
         try:
             attr = getattr(self, val_name)
         except AttributeError:
+            if throws:
+                raise SureValueError()
+
             setattr(self, val_name, Consts.Undefined)
+
 
         if attr is Consts.Undefined:
             try:
@@ -47,7 +82,7 @@ def gen_prop_setter(val_name, typ, throws):
         if value is not Consts.Fail:
             setattr(self, val_name, value)
         elif value is Consts.Fail:
-            u_resolve_fail(throws)
+            return u_resolve_fail(throws)
 
     return _internal
 
@@ -56,11 +91,83 @@ def prop(typ, throws=True):
         generates a random name for the property
         and sets up the setter and getter
     """
-    val_name = str(randrange(10000))
-    val_name += "{0:.20f}".format(time()).replace(".", "")
-    val_name = "_var" + val_name
+    val_name = _gen_random_name()
 
     setter = gen_prop_setter(val_name, typ, throws)
     getter = gen_prop_getter(val_name, setter, throws)
 
     return property(getter, setter)
+
+def gen_nested_prop_getter(val_name, throws, klass):
+    """
+        generates a nested property getter, it
+        actually returns an _Internal object
+    """
+
+    def _internal(self):
+        try:
+            getattr(self, val_name)
+        except AttributeError:
+            setattr(self, val_name, klass())
+
+        return getattr(self, val_name)
+
+    return _internal
+
+def gen_nested_prop_setter(val_name, throws, klass):
+    """
+        generates a nested property setter,
+        it basically linearly tests and assigns
+    """
+
+    def _internal(self, value):
+        assert isinstance(value, dict)
+
+        try:
+            getattr(self, val_name)
+        except AttributeError:
+            setattr(self, val_name, klass())
+
+        this = getattr(self, val_name)
+        for elm, val in value.iteritems():
+            try:
+                setattr(this, elm, val)
+            except SureTypeError():
+                setattr(self, val_name, this)
+
+                if throws:
+                    return Consts.Fail
+                else:
+                    raise SureTypeError()
+
+    return _internal
+
+# nested prop indicates nested stuff
+def nested_prop(nestd, throws=True):
+    """
+        helps build a nested property map
+    """
+    assert isinstance(nestd, dict)
+    val_name = _gen_random_name()
+
+    # generate a struct class
+    class _InternalStruct(object): pass
+
+    for elm, typ in nestd.iteritems():
+        if isinstance(typ, property):
+            setattr(_InternalStruct, elm, typ)
+        else:
+            assert callable(typ), "TypeDefinitionError"
+            setattr(_InternalStruct, elm, prop(typ, True))
+
+    setter = gen_nested_prop_setter(val_name, throws, _InternalStruct)
+    getter = gen_nested_prop_getter(val_name, throws, _InternalStruct)
+    return property(getter, setter)
+
+def nested(nestd, throws=True):
+    """
+        helps you build a nested inside a nested
+        prop, it basically acts liek that too
+    """
+
+    return nested_prop(nestd, throws)
